dbc9e12d7da18a16b549941c1cacfed663ef382a
Kartar Singh <k4kartar4520@gmail.com>
2023-09-12 17:07:02 +0530

    added validations

--- routes/auth.js
+++ routes/auth.js
@@ -1,13 +1,25 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/Users');
+const { body, validationResult } = require('express-validator');
 
 // Creating a User using :post api - no auth required
-router.post('/',(req,res)=>{
-    console.log(req.body);
-    const user = User(req.body);
-    user.save();
-    res.send(req.body);
+router.post('/',[
+    body('name','Enter a valid name').isLength({min:3}),
+    body('email', 'Enter a valid Email').isEmail(),
+    body('password', 'Password must be atleast 8 characters').isLength({min:8}),
+    ],
+    (req,res)=>{
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({ errors: errors.array() });
+    }
+    User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+    }).then(user => res.json(user)).catch(err=> {console.log(err)
+    res.json({error:'Please enter a unique value', message: err.message})});
 })
 
 module.exports = router;
